1cf19a5d1b24f48ac58887cdbb17fdd13d7506f7
omkarhcode <omkarh.code@gmail.com>
2025-01-08 14:53:06 +0530

    added console log

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  console.info('🚀 Backend server is running on port 3000');
 }
 bootstrap();
