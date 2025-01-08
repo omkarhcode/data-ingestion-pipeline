7491a284b53246bd5cb95c587430d7b93fe456f2
omkarhcode <omkarh.code@gmail.com>
2025-01-08 14:54:40 +0530

    added resource Record

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from 'nestjs-prisma';
+import { RecordsModule } from './records/records.module';
 
 @Module({
   imports: [
     PrismaModule.forRoot({
       isGlobal: true,
     }),
+    RecordsModule,
   ],
   controllers: [],
   providers: [],
