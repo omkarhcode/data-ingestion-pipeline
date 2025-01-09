86855058847c6c147046144ee5ea565111c03b68
omkarhcode <omkarh.code@gmail.com>
2025-01-09 12:47:34 +0530

    added global validations

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // enableDebugMessages: true, // If set to true, validator will print extra warning messages to the console when something is not right.
+      // skipUndefinedProperties: true, // If set to true then validator will skip validation of all properties that are undefined in the validating object.
+      // skipNullProperties: true, // If set to true then validator will skip validation of all properties that are null in the validating object.
+      // skipMissingProperties: true, // If set to true then validator will skip validation of all properties that are null or undefined in the validating object.
+      whitelist: true, // If set to true, validator will strip validated (returned) object of any properties that do not use any validation decorators.
+      forbidNonWhitelisted: true, // If set to true, instead of stripping non-whitelisted properties validator will throw an exception.
+      // forbidUnknownValues: true, // If set to true, attempts to validate unknown objects fail immediately.
+      transform: true,
+    }),
+  );
   await app.listen(3000);
   console.info('🚀 Backend server is running on port 3000');
 }
