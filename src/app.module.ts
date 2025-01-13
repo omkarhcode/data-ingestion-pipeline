eb6b65a47f3a25790c994c2f0cb033c82d4958a4
omkarhcode <omkarh.code@gmail.com>
2025-01-13 15:13:12 +0530

    added commented code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from 'nestjs-prisma';
 import { RecordsModule } from './records/records.module';
+// import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
   imports: [
+    // MulterModule.register({
+    //   limits: {
+    //     fileSize: 10 * 1024 * 1024, // 10 MB
+    //   },
+    // }),
     PrismaModule.forRoot({
       isGlobal: true,
     }),
