d3a9a29f32227c9241f86295d40baf76f1f5e8da
omkarhcode <omkarh.code@gmail.com>
2025-01-13 15:13:42 +0530

    created upload-json API

--- src/records/records.controller.ts
+++ src/records/records.controller.ts
@@ -6,12 +6,18 @@ import {
   Patch,
   Param,
   Delete,
-  ValidationPipe,
+  UseInterceptors,
+  BadRequestException,
+  UploadedFile,
 } from '@nestjs/common';
 import { RecordsService } from './records.service';
 import { CreateRecordDto } from './dto/create-record.dto';
 import { UpdateRecordDto } from './dto/update-record.dto';
 import { CreateMultipleRecordDto } from './dto/create-multiple-record.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { memoryStorage } from 'multer';
+import { plainToInstance } from 'class-transformer';
+import { validate } from 'class-validator';
 
 @Controller('records')
 export class RecordsController {
@@ -31,6 +37,52 @@ export class RecordsController {
     createMultipleRecordDto: CreateMultipleRecordDto,
   ) {
     return await this.recordsService.createMany(createMultipleRecordDto);
+  }
+
+  @Post('upload-json')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: memoryStorage(),
+      limits: {
+        fileSize: 10 * 1024 * 1024, // 10 MB
+      },
+      fileFilter: (req, file, callback) => {
+        if (!file.originalname.toLowerCase().endsWith('.json')) {
+          return callback(
+            new BadRequestException('Only .json files are allowed'),
+            false,
+          );
+        }
+        callback(null, true);
+      },
+    }),
+  )
+  async uploadJsonFile(@UploadedFile() file: Express.Multer.File) {
+    if (!file) {
+      throw new BadRequestException('No file provided');
+    }
+
+    let raw: any;
+    try {
+      raw = JSON.parse(file.buffer.toString());
+    } catch (err) {
+      throw new BadRequestException('Invalid JSON structure');
+    }
+
+    const dto = new CreateMultipleRecordDto();
+    dto.records = raw.records;
+
+    const createMultipleRecordDto = plainToInstance(
+      CreateMultipleRecordDto,
+      dto,
+    );
+
+    const errors = await validate(createMultipleRecordDto);
+    if (errors.length > 0) {
+      throw new BadRequestException(errors.toString());
+    }
+
+    return this.recordsService.createMany(createMultipleRecordDto);
   }
 
   @Get()
